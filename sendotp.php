e01e5977aab2ef96e6d74be4b6b818f705923222
bmr3256 <bryanmiguel.ramos.cics@ust.edu.ph>
2026-03-25 20:15:19 +0800

    Update sendotp.php
    3-25-2026
    
    update

--- sendotp.php
+++ sendotp.php
@@ -4,8 +4,13 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
 require 'vendor/autoload.php';
-
 session_start();
+
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    http_response_code(405);
+    echo 'METHOD_NOT_ALLOWED';
+    exit;
+}
 
 $email = $_POST['email'];
 
@@ -18,14 +23,24 @@ $_SESSION['otp_time'] = time();
 $mail = new PHPMailer(true);
 
 try {
-
     $mail->isSMTP();
+    $mail->SMTPDebug = 0; // remove debug output from response
+    $mail->SMTPAutoTLS = false;
+    $mail->SMTPSecure = 'tls'; // or 'ssl'
+    $mail->Port = 587;        // or 465 for ssl
+
+    $mail->SMTPOptions = [
+        'ssl' => [
+            'verify_peer'       => false,
+            'verify_peer_name'  => false,
+            'allow_self_signed' => true,
+        ],
+    ];
+
     $mail->Host = 'smtp.gmail.com';
     $mail->SMTPAuth = true;
     $mail->Username = 'bryanmiguel.ramos.cics@ust.edu.ph';
     $mail->Password = 'jsjx dunr vjwu lpki';
-    $mail->SMTPSecure = 'tls';
-    $mail->Port = 587;
 
     $mail->setFrom('bryanmiguel.ramos.cics@ust.edu.ph', 'The Barkery');
     $mail->addAddress($email);
@@ -43,5 +58,7 @@ try {
 
     echo "OTP_SENT";
 } catch (Exception $e) {
-    echo "ERROR";
+    http_response_code(500);
+    // Reveal the mailer error for debugging (remove or hide in production)
+    echo "ERROR: " . $mail->ErrorInfo . " | " . $e->getMessage();
 }
